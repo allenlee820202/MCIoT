a09a2520636f89ee86e63158fd6ede146b3b0e2f
allenlee820202 <allenlee820202@gmail.com>
2017-07-11 07:18:33 +0000

    modify april tag monitor

--- PicameraDetectApriltags/monitor.py
+++ PicameraDetectApriltags/monitor.py
@@ -1,7 +1,20 @@
 import picamera
-from subprocess import call
+import subprocess 
+import os
+
+image_name = 'image.jpg'
+path_name = os.path.dirname(os.path.abspath(__file__))
+
+command = path_name+"/apriltags_demo -d "+path_name+"/"+image_name
 
 camera = picamera.PiCamera()
 
-camera.capture('image.jpg')
-call(["/home/pi/PicameraDetectApriltags/apriltags_demo", "/home/pi/PicameraDetectApriltags/image.jpg"])
+camera.capture(image_name)
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+out, err = process.communicate()
+errcode = process.returncode
+
+print out
+
+print err
