e2c0a8076be5c0e1c2dc3561efb0c56fa3359b32
allenlee820202 <allenlee820202@gmail.com>
2017-07-17 14:03:10 +0000

    modify monitor.py

--- PicameraDetectApriltags/monitor.py
+++ PicameraDetectApriltags/monitor.py
@@ -1,20 +1,47 @@
 import picamera
 import subprocess 
 import os
+import time, threading
 
-image_name = 'image.jpg'
 path_name = os.path.dirname(os.path.abspath(__file__))
 
-command = path_name+"/apriltags_demo -d "+path_name+"/"+image_name
+class Monitor_t(object):
+	camera = picamera.PiCamera()
+	isStart = False
 
-camera = picamera.PiCamera()
+	def take_shot(self):
+		image_name = 'image.jpg'
+		command = path_name+"/apriltags_demo -d "+path_name+"/"+image_name
 
-camera.capture(image_name)
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.camera.capture(image_name)
+		process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = process.communicate()
+		errcode = process.returncode
+		return out
+		
+	def processing_data(self, raw_data):
+		print raw_data
+		for line in raw_data:
+			return line
 
-out, err = process.communicate()
-errcode = process.returncode
+	def daemon_thread_function(self):
+		while(self.isStart):
+			print self.processing_data( self.take_shot() )
+			time.sleep(1)
+		
+	def start(self):
+		self.isStart=True
+		self.daemon_t = threading.Thread(target = self.daemon_thread_function, args = (), name = 'daemon')
+		self.daemon_t.start()
 
-print out
-
-print err
+	def stop(self):
+		self.isStart=False
+		self.daemon_t.join()
+		
+	def __init__(self):
+		self.start()
+		time.sleep(10)
+		self.stop()
+	
+if __name__ == '__main__':
+	Monitor_t()
