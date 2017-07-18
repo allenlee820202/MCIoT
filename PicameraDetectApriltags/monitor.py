dc5287005747bd32f6d5bee0a473e294cbbc4a05
allenlee820202 <allenlee820202@gmail.com>
2017-07-18 12:34:18 +0000

    modify monitor.py

--- PicameraDetectApriltags/monitor.py
+++ PicameraDetectApriltags/monitor.py
@@ -49,34 +49,36 @@ class Monitor_t(object):
 		w1 = np.subtract(m1,m0).tolist()
 		w2 = np.subtract(m2,m0).tolist()
 		# in pixel coordination
-		print self.mark["0"]
+		# print self.mark["0"]
 		t0 = self.mark["0"] # choose origin point
 		t1 = self.mark["1"] # choose origin point
 		t2 = self.mark["2"] # choose origin point
 		v1 = np.subtract(t1, t0).tolist()
 		v2 = np.subtract(t2, t0).tolist()
-		#print v1, v2
+		# print v1, v2
 		A = np.asmatrix([[v1[0], v1[1]], [v2[0], v2[1]]]).T
-		print A
+		# print A
+		# calculation phase
+		ret = {}
 		for tid, o in self.obj.iteritems():
 			b = np.subtract(np.asarray(o), t0).T
 			r = np.linalg.solve(A,b)
-			print b
-			print r
-			print w1, w2
+			# print b
+			# print r
+			# print w1, w2
 			y = r[0]*np.asarray(w1) + r[1]*np.asarray(w2)
 			y[0] = y[0]+m0[0]
 			y[1] = y[1]+m0[1]
 			
-			print y
+			ret[tid] = y
+		return ret
 				
 
-		# calculation phase
 
 	def daemon_thread_function(self):
 		while(self.isStart):
 			self.processing_image( self.take_shot() )
-			self.calculating_coordinations()
+			print self.calculating_coordinations()
 			time.sleep(1)
 		
 	def start(self):
