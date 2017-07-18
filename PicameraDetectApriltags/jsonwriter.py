c5afb769a5e94c7afdceddaa0f5cc91864e89a62
allenlee820202 <allenlee820202@gmail.com>
2017-07-18 12:01:27 +0000

    monitor.py: implement coordination transformation

---
+++ PicameraDetectApriltags/jsonwriter.py
@@ -0,0 +1,15 @@
+import json
+
+data = {
+	'0':'(0, 0)',
+	'1':'(0, 10)',
+	'2':'(20, 0)'
+} 
+
+json_str = json.dumps(data)
+
+with open('config.json', 'w') as f:
+	json.dump(json_str, f)
+
+with open('config.json', 'r') as f:
+	print json.load(f)
--- PicameraDetectApriltags/monitor.py
+++ PicameraDetectApriltags/monitor.py
@@ -2,12 +2,18 @@ import picamera
 import subprocess 
 import os
 import time, threading
+import json
+import ast
+import numpy as np
 
 path_name = os.path.dirname(os.path.abspath(__file__))
 
 class Monitor_t(object):
 	camera = picamera.PiCamera()
 	isStart = False
+	config = ""		# user specified coordinations of marks loaded from config.json
+	mark = {}		# pixel coordinations of marks
+	obj = {}		# pixel coordinations of objects
 
 	def take_shot(self):
 		image_name = 'image.jpg'
@@ -19,14 +25,58 @@ class Monitor_t(object):
 		errcode = process.returncode
 		return out
 		
-	def processing_data(self, raw_data):
-		print raw_data
-		for line in raw_data:
-			return line
+	def processing_image(self, raw_data):
+		if raw_data.split('\n')[1].split(' ')[0] != '0':
+			for line in raw_data.split('\n')[2:-1]:
+				tid = line.split(' ')[3]
+				x = line.split(' ')[6].split('(')[1]
+				y = line.split(' ')[7]
+				#print tid, x, y
+				if tid in self.config.keys():
+					self.mark[tid] = (float(x), float(y))
+				else:
+					self.obj[tid] = (float(x), float(y))
+				
+		else:
+			print 'nothing detected'
+
+	def calculating_coordinations(self):
+		# initialization phase
+		# in user specified coordinations of marks
+		m0 = self.config["0"]
+		m1 = self.config["1"]
+		m2 = self.config["2"]
+		w1 = np.subtract(m1,m0).tolist()
+		w2 = np.subtract(m2,m0).tolist()
+		# in pixel coordination
+		print self.mark["0"]
+		t0 = self.mark["0"] # choose origin point
+		t1 = self.mark["1"] # choose origin point
+		t2 = self.mark["2"] # choose origin point
+		v1 = np.subtract(t1, t0).tolist()
+		v2 = np.subtract(t2, t0).tolist()
+		#print v1, v2
+		A = np.asmatrix([[v1[0], v1[1]], [v2[0], v2[1]]]).T
+		print A
+		for tid, o in self.obj.iteritems():
+			b = np.subtract(np.asarray(o), t0).T
+			r = np.linalg.solve(A,b)
+			print b
+			print r
+			print w1, w2
+			y = r[0]*np.asarray(w1) + r[1]*np.asarray(w2)
+			y[0] = y[0]+m0[0]
+			y[1] = y[1]+m0[1]
+			
+			print y
+				
+
+		# calculation phase
 
 	def daemon_thread_function(self):
 		while(self.isStart):
-			print self.processing_data( self.take_shot() )
+			self.processing_image( self.take_shot() )
+			self.calculating_coordinations()
 			time.sleep(1)
 		
 	def start(self):
@@ -39,9 +89,15 @@ class Monitor_t(object):
 		self.daemon_t.join()
 		
 	def __init__(self):
-		self.start()
-		time.sleep(10)
-		self.stop()
-	
+		with open('config.json', 'r') as f:
+			self.config = ast.literal_eval( json.load(f) )
+			
+		for key, value in self.config.iteritems():
+			self.config[key] = ast.literal_eval(value)	# parse value from string type to tuple type
+			# print key, self.config[key]
+
 if __name__ == '__main__':
-	Monitor_t()
+	monitor = Monitor_t()
+	monitor.start()
+	time.sleep(5)
+	monitor.stop()
